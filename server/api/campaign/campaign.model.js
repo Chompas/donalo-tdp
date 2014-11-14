ea491694ec6e188d2a682ed134948bd5f401c70a
Emiliano Viscarra <chompas@gmail.com>
2014-11-14 15:42:30 -0300

    Agrego modelo de campañas

---
+++ server/api/campaign/campaign.model.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var CampaignSchema = new Schema({
+  name: String,
+  info: String,
+  created: Date,
+  expires: Date,
+  campaignType: Number,
+  totalAmount: Number,
+  currentAmount: Number,
+  active: Boolean
+});
+
+module.exports = mongoose.model('Campaign', CampaignSchema);
