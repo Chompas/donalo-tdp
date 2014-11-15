a5e6cfca9e566f5f33622972b5472a5f1f0547e9
Emiliano Viscarra <chompas@gmail.com>
2014-11-15 15:30:00 -0300

    Agrego baja de campañas

--- client/app/admin/campaigns/campaigns.controller.js
+++ client/app/admin/campaigns/campaigns.controller.js
@@ -6,4 +6,13 @@ angular.module('donaloTdpApp')
     // Use the Campaign $resource to fetch all users
     $scope.campaigns = Campaign.query();
 
+    $scope.delete = function(user) {
+      Campaign.remove({ id: user._id });
+      angular.forEach($scope.campaigns, function(u, i) {
+        if (u === user) {
+          $scope.campaigns.splice(i, 1);
+        }
+      });
+    };
+
   });
