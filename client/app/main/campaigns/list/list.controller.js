823c6de44d284034726626767339b500eb90aa5b
Emiliano Viscarra <chompas@gmail.com>
2014-11-24 16:45:08 -0300

    Agrego listado de campaigns al controller de list

--- client/app/main/campaigns/list/list.controller.js
+++ client/app/main/campaigns/list/list.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
 angular.module('donaloTdpApp')
-  .controller('ListCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ListCtrl', function ($scope, $http, Campaign) {
+
+    // Use the Campaign $resource to fetch all campaigns
+    $scope.campaigns = Campaign.query();
   });
