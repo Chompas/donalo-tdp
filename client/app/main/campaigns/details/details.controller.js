7c70a774abeacb485a26bbae4a36494c3a01ab46
unknown <gisedaye@gmail.com>
2014-11-24 20:23:48 -0300

    Direcciones a coordenadas

--- client/app/main/campaigns/details/details.controller.js
+++ client/app/main/campaigns/details/details.controller.js
@@ -1,15 +1,18 @@
 'use strict';
 
 angular.module('donaloTdpApp')
-  .controller('DetailsCtrl', function ($scope, $routeParams, Campaign, User) {
+  .controller('DetailsCtrl', function ($scope,$http, $routeParams, Campaign, User) {
+
     var campaign = Campaign.show({id: $routeParams.id});
+    //$scope.map  = { center: { latitude: 54, longitude: -73 }, zoom: 8 };
     campaign.$promise.then(function(data){
       var expire = new Date(data.expires);
       var now = new Date();
       var diff =  Math.abs(expire - now);
-
+      var directions = (data.address).replace(' ','+');
       var seconds = Math.floor(diff/1000); //ignore any left over units smaller than a second
       var minutes = Math.floor(seconds/60);
+
       seconds = seconds % 60;
       var hours = Math.floor(minutes/60);
       minutes = minutes % 60;
@@ -20,7 +23,13 @@ angular.module('donaloTdpApp')
         $scope.expire = hours + " horas " + minutes + " minutos ";
       }
       $scope.campaign = data;
-      $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+      $.getJSON('http://maps.google.com/maps/api/geocode/json?address='+directions, function(data){
+        var location = data.results[0].geometry.location;
+        //TODO que el scope pase a jade
+        $scope.map  = { center: { latitude: location.lat, longitude: location.lng }, zoom: 8 };
+      });
+
       // var user = User.show({id: data.userId});
       // user.$promise.then(function(userData){
       //   $scope.user = userData;
