179f1c01bb842fd7ec4521cee2d5136f3fbd6315
Oscar Bertran <oabertran@gmail.com>
2014-12-11 01:19:04 -0300

    Agrego el frontend de campaign list

--- client/app/main/campaigns/list/list.controller.js
+++ client/app/main/campaigns/list/list.controller.js
@@ -1,8 +1,78 @@
-'use strict';
+  'use strict';
 
-angular.module('donaloTdpApp')
-  .controller('ListCtrl', function ($scope, $http, Campaign) {
+  angular.module('donaloTdpApp')
+  .controller('ListCtrl', function ($scope, $http, Campaign,uiGmapGoogleMapApi,$timeout) {
 
-    // Use the Campaign $resource to fetch all campaigns
-    $scope.campaigns = Campaign.query();
+    $scope.campaignType = ['Monetaria','Bienes','Voluntarios'];
+
+    var maps = [[],[],[]];
+
+    var latlngBounds = [new google.maps.LatLngBounds(),new google.maps.LatLngBounds(),new google.maps.LatLngBounds()];
+
+    uiGmapGoogleMapApi.then(function(maps){
+
+      Campaign.query().$promise.then(function(campaignsData){
+
+        campaignsData.progress = function(campaign){
+
+          return (campaign.currentAmount / campaign.totalAmount)*100;
+        };
+
+        var bounds = [];
+        var markers = [];
+
+        angular.forEach(campaignsData,function(campaignValue,campaignKey){
+
+          if (!(campaignValue.campaignType in bounds)){
+
+            bounds[campaignValue.campaignType] = new google.maps.LatLngBounds();
+            markers[campaignValue.campaignType] = [];
+          }
+
+          var latlng = new google.maps.LatLng(campaignValue.coords.latitude, campaignValue.coords.longitude);
+
+          bounds[campaignValue.campaignType].extend(latlng);
+
+          markers[campaignValue.campaignType].push( {
+            coords:{
+              latitude: campaignValue.coords.latitude,
+              longitude: campaignValue.coords.longitude
+            },
+            id: 'marker-'+ campaignKey
+          });
+
+        });
+
+        $scope.markers = markers;
+
+        $scope.campaigns = campaignsData;
+
+        var map = [];
+        angular.forEach($scope.campaignType,function(value,key){
+
+          if ((key in bounds)){
+            map[key] = {center: { latitude: bounds[key].getCenter().lat(), longitude: bounds[key].getCenter().lng() }, zoom: 10};
+
+          }
+
+        });
+
+        $scope.maps = map;
+
+        $scope.active = function(index){
+
+          $timeout(function(){
+            var mapa = angular.element(document.querySelector('#maps-'+ index + ' .angular-google-map')).scope().map;
+
+            if (mapa){
+
+              google.maps.event.trigger(mapa, 'resize');
+              mapa.setCenter(new google.maps.LatLng($scope.maps[index].center.latitude,$scope.maps[index].center.longitude));
+            }
+          });
+        }
+      });
+
+    });
+
   });
--- server/api/campaign/campaign.model.js
+++ server/api/campaign/campaign.model.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+Schema = mongoose.Schema;
 
 var CampaignSchema = new Schema({
   name: String,
   info: String,
   imageUrl: String,
   address: String,
+  coords: Object,
   created: Date,
   expires: Date,
   campaignType: Number,
@@ -15,6 +16,25 @@ var CampaignSchema = new Schema({
   currentAmount: Number,
   active: Boolean,
   userId: String
+});
+
+CampaignSchema.pre('save', function (next) {
+
+  var gm = require('geocoder');
+  var self = this;
+  self.coords = {};
+  gm.geocode(self.address, function(err, data) {
+
+    var result = data.results;
+
+    if (result && result[0]){
+
+      self.coords = {latitude: result[0].geometry.location.lat, longitude: result[0].geometry.location.lng};
+    }
+
+    next();
+  });
+
 });
 
 module.exports = mongoose.model('Campaign', CampaignSchema);
